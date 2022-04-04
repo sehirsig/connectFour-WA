4510ebe80516a30c0fb8472fc09ab1405039fe9d
sehirsig <sebastianhirsig@bluewin.ch>
2022-04-04 16:07:51 +0200

    Shortened the gui win function alot.

--- src/main/scala/de/htwg/se/ConnectFour/aUI/states/GUI/WinState.scala
+++ src/main/scala/de/htwg/se/ConnectFour/aUI/states/GUI/WinState.scala
@@ -5,21 +5,22 @@ import de.htwg.se.ConnectFour.controller.Controller
 import scalafx.scene.control.Alert
 import scalafx.scene.control.Alert.AlertType
 
+import scala.io.AnsiColor
+
 /**
  * WinState of GUI
  */
 case class WinState(controller: Controller) extends State[GameState]:
+  //Extension Methods
+  extension (con:Controller)
+    def printWinner:Unit =
+      new Alert(AlertType.Information){ title = "We have a Winner!"
+        headerText = "Congratulations " + controller.currentPlayer.playerName + "!!!"
+        contentText = "You are the winner of this round! :)"}.showAndWait()
+
   override def handle(input: String, gui: GUI, state: GameState) =
     if input == "won" then
-      if controller.currentPlayer.color.equals("red") then
-        new Alert(AlertType.Information){ title = "We have a Winner!"
-          headerText = "Congratulations " + controller.currentPlayer.playerName + "!!!"
-          contentText = "you are the winner of this round! :)"}.showAndWait()
-      else
-        new Alert(AlertType.Information){ title = "We have a Winner!"
-          headerText = "Congratulations " + controller.currentPlayer.playerName + "!!!"
-          contentText = "you are the winner of this round! :)"}.showAndWait()
-      end if
+      controller.printWinner
     else if (input == "n")
       controller.reset()
       state.changeState(DropState(controller))
