42017688672864e69036a32b16be2906ff0699b2
sehirsig <sebastianhirsig@bluewin.ch>
2022-04-09 23:43:18 +0200

    Abgabe 04. FileIO Auskommentiert, da Fehler mit Zugriff auf Dependencys. (Scheint normal zu sein, nächste Abgabe sollte das regeln).

--- src/main/scala/de/htwg/se/ConnectFour/ConnectFourModule.scala
+++ src/main/scala/de/htwg/se/ConnectFour/ConnectFourModule.scala
@@ -4,14 +4,14 @@ import com.google.inject.AbstractModule
 import com.google.inject.name.Names
 import de.htwg.se.ConnectFour.controller.controllerComponent.ControllerInterface
 import de.htwg.se.ConnectFour.controller.controllerComponent.controllerBaseImpl.Controller
-import de.htwg.se.ConnectFour.model.fileIOComponent.FileIOInterface
-import de.htwg.se.ConnectFour.model.fileIOComponent.*
-import de.htwg.se.ConnectFour.model.gridComponent.GridInterface
-import de.htwg.se.ConnectFour.model.gridComponent.gridBaseImpl.Grid
-import de.htwg.se.ConnectFour.model.playerComponent.PlayerBuilderInterface
-import de.htwg.se.ConnectFour.model.playerComponent.playerBuilderBaseImpl.PlayerBuilder
-import de.htwg.se.ConnectFour.model.playerComponent.PlayerInterface
-import de.htwg.se.ConnectFour.model.playerComponent.playerBaseImpl.Player
+//import fileIOComponent.FileIOInterface
+//import fileIOComponent.*
+import model.gridComponent.GridInterface
+import model.gridComponent.gridBaseImpl.Grid
+import model.playerComponent.PlayerBuilderInterface
+import model.playerComponent.playerBuilderBaseImpl.PlayerBuilder
+import model.playerComponent.PlayerInterface
+import model.playerComponent.playerBaseImpl.Player
 import net.codingwell.scalaguice.ScalaModule
 
 /**
@@ -27,6 +27,6 @@ class ConnectFourModule extends AbstractModule:
     bind(classOf[PlayerInterface]).to(classOf[Player])
     bind(classOf[PlayerBuilderInterface]).to(classOf[PlayerBuilder])
     bind(classOf[GridInterface]).to(classOf[Grid])
-    bind(classOf[FileIOInterface]).to(classOf[json.FileIO])
+    //bind(classOf[FileIOInterface]).to(classOf[json.FileIO])
     //bind(classOf[FileIO]).to(classOf[xml.FileIOImpl])
     bind(classOf[ControllerInterface]).to(classOf[Controller])
