d71a416a0cb3c84dde88f2e0643e0302a64df0f6
furkankarayel <furkarayel@hotmail.com>
2022-05-09 21:22:24 +0200

    Slick Delete working

--- Database/src/main/scala/de/htwg/se/ConnectFour/databaseComponent/Database.scala
+++ Database/src/main/scala/de/htwg/se/ConnectFour/databaseComponent/Database.scala
@@ -27,5 +27,9 @@ object Database {
     print("\nPlayer Color: " + result.get._3)
     print("\nPlayer Name: " + result.get._4)
     StdIn.readLine() // let it run until user presses return
+    val deletePlayer1 = slick.deletePlayer(1)
+    print("Deletion of player 1 succeded: " + deletePlayer1.isCompleted + "\n")
+    Thread.sleep(10000)
+    print("Deletion of player 1 succeded: " + deletePlayer1.isCompleted + "\n")
   }
 }
