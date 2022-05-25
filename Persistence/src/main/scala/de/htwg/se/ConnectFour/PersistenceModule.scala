520909c32d71c399b12c5a233dfd4d22ef8ae581
furkankarayel <furkarayel@hotmail.com>
2022-05-25 03:04:05 +0200

    Volume Test Pre & Future Slick added to DAOLoadSave

--- Persistence/src/main/scala/de/htwg/se/ConnectFour/PersistenceModule.scala
+++ Persistence/src/main/scala/de/htwg/se/ConnectFour/PersistenceModule.scala
@@ -15,4 +15,4 @@ class PersistenceModule extends AbstractModule:
     //bind(classOf[FileIOInterface]).to(classOf[xml.FileIO])
     bind(classOf[DBInterface]).to(classOf[DBSlickImpl])
     //bind(classOf[DAOInterface]).to(classOf[DAOSlickGridPlayerImpl])
-    bind(classOf[DAOInterface]).to(classOf[])
+    bind(classOf[DAOInterface]).to(classOf[DAOSlickGridPlayerImpl])
