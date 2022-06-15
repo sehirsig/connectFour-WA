0936009e39419a582b1f87eb46bd0b0b8ab0c238
sehirsig <81407658+sehirsig@users.noreply.github.com>
2022-06-15 23:24:59 +0200

    Hotfix: deleted comment

--- Persistence/src/main/scala/de/htwg/se/ConnectFour/databaseComponent/Slick/tables/SettingsTable.scala
+++ Persistence/src/main/scala/de/htwg/se/ConnectFour/databaseComponent/Slick/tables/SettingsTable.scala
@@ -11,8 +11,5 @@ class SettingsTable(tag: Tag) extends Table[(Int, String, Int)](tag, "SETTINGS")
   def currplayer = column[String]("currplayer")
   def movecount = column[Int]("movecount")
 
-  //def userFK = //Foreign Key to join Player and Settings Current Player
-    //foreignKey("currplayer_fk", currplayer, TableQuery[PlayerTable])(_.name, ForeignKeyAction.Restrict, ForeignKeyAction.Cascade)
-
   override def * = (id, currplayer, movecount)
 }
